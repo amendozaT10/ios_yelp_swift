d72e7e8d456856e38f43910885a5bb3b7c49c91e
Alejandro Mendoza <Alejandro_Mendoza@intuit.com>
2017-04-06 22:32:04 -0700

    Removed the label width detting calls.

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -38,12 +38,12 @@ class BusinessCell: UITableViewCell {
         thumbImageView.layer.cornerRadius = 5
         thumbImageView.clipsToBounds = true
         
-        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        //nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        //nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
